25e7aa5a92bf344f633933aa5c9abf91d4e45768
Yao Kaiser <accounts@ywk.ch>
2022-05-19 15:50:28 +0200

    init

---
+++ src/main/java/ch/bbw/mygourmet/MyGourmetM431Application.java
@@ -0,0 +1,13 @@
+package ch.bbw.mygourmet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGourmetM431Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGourmetM431Application.class, args);
+	}
+
+}
