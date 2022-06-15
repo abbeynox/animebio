f71f4c7c023c16fa3ed9207feb4f51ed2ced3df6
Yao Kaiser <dobie_lilesville@relay.ywk.ch>
2022-06-15 23:16:08 +0200

    refactor: add images

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,12 +6,11 @@ import Vuesax from 'vuesax'
 
 Vue.use(Vuesax, {
   colors: { // TODO: Search for color guidelines -> Jade
-    primary:'#5b3cc4',
-    success:'rgb(23, 201, 100)',
-    danger:'rgb(242, 19, 93)',
-    warning:'rgb(255, 130, 0)',
-    dark:'rgb(36, 33, 69)',
-    light:'rgb(255,255,255)'
+    primary:'#BC73FF', // YWK Purple
+    success:'#15b75b', // Green
+    danger:'#ff3300', // Red
+    warning:'#ff8200', // Orange
+    dark:'#242145',
   }
 })
 
