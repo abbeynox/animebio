3bea3a46f7c39361bf47e598af7fe57abc4d3073
kaiseryao <dobie_lilesville@relay.ywk.ch>
2022-06-20 23:44:23 +0200

    feature: implement axios

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,10 +3,12 @@ import 'vuesax/dist/vuesax.css'
 import App from './App.vue'
 import Vue from 'vue'
 import VueRouter from 'vue-router';
-import Vuesax from 'vuesax'
-import router from './routes/'
+import Vuesax from 'vuesax';
+import router from './routes/';
+import VueAxios from 'vue-axios';
 
 Vue.use(VueRouter);
+Vue.use (VueAxios);
 
 Vue.use(Vuesax, {
   colors: { // TODO: Search for color guidelines -> Jade
